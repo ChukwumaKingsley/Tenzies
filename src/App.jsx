a574e215245618bb995960a2f8dc2737b9711dac
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-01 17:19:12 +0100

    Added random numbers to all die

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,17 @@ import './App.css'
 import Die from './Die'
 
 function App() {
+  const setAllNewDie = () =>
+    Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+
+  const [allDie, setAllDie] = useState(setAllNewDie());
 
   return (
     <main>
       <div className='die-container'>
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
-          <Die val={1} />
+      {allDie.map((value, index) => (
+                    <Die key={index} val={value} />
+                    ))}
       </div>
     </main>
   )
