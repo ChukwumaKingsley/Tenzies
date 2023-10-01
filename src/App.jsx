1c66bee63db0c99808d62d48cabf215628571e40
ChukwumaKingsley <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-01 16:50:55 +0100

    Added dice boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import Die from './Die'
 
 function App() {
 
   return (
     <main>
-      grin
+      <div className='die-container'>
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+          <Die val={1} />
+      </div>
     </main>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,9 @@
+
+
+function Die(props) {
+    return (
+        <span className="die">{props.val}</span>
+    )
+}
+
+export default Die
